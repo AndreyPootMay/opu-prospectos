4568d76c0777b774133f87f47b7c18e24c441812
AndreyPootMay <andreypootmay@gmail.com>
2026-07-14 19:03:12 -0500

    release apk

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import App from './App';
 import './index.css';
+
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setOverlaysWebView({ overlay: true });
+  StatusBar.setStyle({ style: Style.Dark });
+  StatusBar.setBackgroundColor({ color: '#00000000' });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
